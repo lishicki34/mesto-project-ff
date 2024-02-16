0068c330c4aab7e0e4180f0621eec717820feff3
lishicki <lishicki34@mail.ru>
2024-02-17 00:33:44 +0300

    17.020033

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/lishicki34/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(initialCards, deleteCards) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = initialCards.link;
+  cardElement.querySelector('.card__title').textContent = initialCards.name;
+
+  deleteButton.addEventListener("click",() => {
+    deleteCard(cardElement)
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+};
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(createCard(item));
+});
